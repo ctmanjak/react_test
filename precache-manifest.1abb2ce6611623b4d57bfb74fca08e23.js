0805e0742412e3f5c228910961b9b419b51df0bb
ctmanjak <ctmanjak@gmail.com>
2020-01-02 21:36:15 +0900

    Updates

---
+++ precache-manifest.1abb2ce6611623b4d57bfb74fca08e23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df3d42883c5f67a96a0aa79b81f63952",
+    "url": "/react_test/index.html"
+  },
+  {
+    "revision": "fc3cebdd4c0a2f92ee2a",
+    "url": "/react_test/static/css/main.701a7a82.chunk.css"
+  },
+  {
+    "revision": "68149808b2e1a089d796",
+    "url": "/react_test/static/js/2.90008685.chunk.js"
+  },
+  {
+    "revision": "99df457ec02847572eac037dc3cb0e2a",
+    "url": "/react_test/static/js/2.90008685.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc3cebdd4c0a2f92ee2a",
+    "url": "/react_test/static/js/main.3daaa136.chunk.js"
+  },
+  {
+    "revision": "dcfaf097dde77f3f26ce",
+    "url": "/react_test/static/js/runtime-main.4293f6ca.js"
+  }
+]);
